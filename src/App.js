8e7d104048874dd862bd77c338026dfe1330e27b
kennya2709 <kennyorona27@gmail.com>
2023-03-23 08:37:16 -0600

    v3

--- src/App.js
+++ src/App.js
@@ -33,11 +33,11 @@ function App() {
 
             <div class="container">
               
-     <div class="row">
-      <div class="col-3"><h1>Metodo render</h1> <br></br>técnica para compartir código entre componentes en React utilizando una propiedad cuyo valor es una función.</div>
-      <div class="col-3"><h1>Metodo strict mode</h1><br></br> simplifica el modo en que el nombre de una variable es asignado a un variable particular en el código.</div>
-      <div class="col-3"><h1>Metodo return </h1><br></br>se emplea para salir de la secuencia de ejecución de las sentencias de un método y, opcionalmente, devolver un valor.</div>
-    </div> 
+      <div class="row">
+        <div class="col-3"><h1>Metodo render</h1> <br></br>técnica para compartir código entre componentes en React utilizando una propiedad cuyo valor es una función.</div>
+        <div class="col-3"><h1>Metodo strict mode</h1><br></br> simplifica el modo en que el nombre de una variable es asignado a un variable particular en el código.</div>
+        <div class="col-3"><h1>Metodo return </h1><br></br>se emplea para salir de la secuencia de ejecución de las sentencias de un método y, opcionalmente, devolver un valor.</div>
+      </div> 
   </div>
   <br></br><br></br><br></br>
               <Button variant="dark" as={Link} to='/Example'>Carrusel</Button>
